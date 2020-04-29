065a099587583ca17906de4cbdfaeae2be033a12
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2020-04-29 16:11:56 +0700

    Update sizes.dart

--- lib/common/sizes.dart
+++ lib/common/sizes.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Sizes {
-
   static height(BuildContext context) {
     return MediaQuery.of(context).size.height;
   }
@@ -11,51 +10,110 @@ class Sizes {
   }
 
   static dp4(BuildContext context) {
-    return MediaQuery.of(context).size.width / 100;
+    return Sizes.width(context) / 100;
+  }
+
+  static dp5(BuildContext context) {
+    return Sizes.width(context) / 83;
   }
 
   static dp6(BuildContext context) {
-    return MediaQuery.of(context).size.width / 70;
+    return Sizes.width(context) / 68;
+  }
+
+  static dp7(BuildContext context) {
+    return Sizes.width(context) / 60;
   }
 
   static dp8(BuildContext context) {
-    return MediaQuery.of(context).size.width / 54;
+    return Sizes.width(context) / 51;
+  }
+
+  static dp9(BuildContext context) {
+    return Sizes.width(context) / 46;
   }
 
   static dp10(BuildContext context) {
-    return MediaQuery.of(context).size.width / 41;
+    return Sizes.width(context) / 41;
+  }
+
+  static dp11(BuildContext context) {
+    return Sizes.width(context) / 38;
   }
 
   static dp12(BuildContext context) {
-    return MediaQuery.of(context).size.width / 34;
+    return Sizes.width(context) / 35;
+  }
+
+  static dp13(BuildContext context) {
+    return Sizes.width(context) / 32;
   }
 
   static dp14(BuildContext context) {
-    return MediaQuery.of(context).size.width / 29;
+    return Sizes.width(context) / 30;
+  }
+
+  static dp15(BuildContext context) {
+    return Sizes.width(context) / 27.5;
   }
 
   static dp16(BuildContext context) {
-    return MediaQuery.of(context).size.width / 26;
+    return Sizes.width(context) / 26;
+  }
+
+  static dp17(BuildContext context) {
+    return Sizes.width(context) / 24.5;
   }
 
   static dp18(BuildContext context) {
-    return MediaQuery.of(context).size.width / 23;
+    return Sizes.width(context) / 23;
+  }
+
+  static dp19(BuildContext context) {
+    return Sizes.width(context) / 22;
   }
 
   static dp20(BuildContext context) {
-    return MediaQuery.of(context).size.width / 20;
+    return Sizes.width(context) / 21;
+  }
+
+  static dp21(BuildContext context) {
+    return Sizes.width(context) / 20;
   }
 
   static dp22(BuildContext context) {
-    return MediaQuery.of(context).size.width / 17;
+    return Sizes.width(context) / 19;
+  }
+
+  static dp23(BuildContext context) {
+    return Sizes.width(context) / 18;
   }
 
   static dp24(BuildContext context) {
-    return MediaQuery.of(context).size.width / 16;
+    return Sizes.width(context) / 17;
   }
 
   static dp25(BuildContext context) {
-    return MediaQuery.of(context).size.width / 15;
+    return Sizes.width(context) / 16.5;
   }
 
+  static dp26(BuildContext context) {
+    return Sizes.width(context) / 16;
+  }
+
+  static dp27(BuildContext context) {
+    return Sizes.width(context) / 15.3;
+  }
+
+  static dp28(BuildContext context) {
+    return Sizes.width(context) / 14.8;
+  }
+
+  static dp29(BuildContext context) {
+    return Sizes.width(context) / 14.3;
+  }
+
+  static dp30(BuildContext context) {
+    return Sizes.width(context) / 13.8;
+  }
 }
