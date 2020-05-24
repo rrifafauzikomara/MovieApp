096d0711d0a9d911ea70dae976e47a24cdc0e786
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2020-05-24 23:35:21 +0700

    add fix ui test

--- shared/lib/src/common/utils/movies_key.dart
+++ shared/lib/src/common/utils/movies_key.dart
@@ -16,14 +16,14 @@ const String KEY_TAP_ITEM_NOW_PLAYING = "tap_movies_338762";
 
 // up coming screen
 const String KEY_LIST_VIEW_UP_COMING = "listview_menu_up_coming";
-const String KEY_TAP_ITEM_UP_COMING = "tap_movies_481848";
+const String KEY_TAP_ITEM_UP_COMING = "tap_movies_514847";
 
 // popular screen
 const String KEY_LIST_VIEW_POPULAR = "listview_menu_popular";
-const String KEY_TAP_ITEM_POPULAR = "tap_movies_545609";
+const String KEY_TAP_ITEM_POPULAR = "tap_movies_419704";
 
 // top rated screen
 const String KEY_LIST_VIEW_TOP = "listview_menu_top";
-const String KEY_TAP_ITEM_TOP = "tap_movies_537061";
+const String KEY_TAP_ITEM_TOP = "tap_movies_640344";
 
 // about screen
