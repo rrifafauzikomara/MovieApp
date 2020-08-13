4e8942f18c850ca6b1ba9760ed3feeb34dc5fdbb
rrifafauzikomara <rifafauzi6@gmail.com>
2020-08-13 23:23:13 +0700

    fix setting screen

--- lib/ui/setting/setting_screen.dart
+++ lib/ui/setting/setting_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared/shared.dart';
+import 'package:package_info/package_info.dart';
 
 class SettingScreen extends StatefulWidget {
   static const routeName = '/theme';
@@ -38,6 +39,11 @@ class _SettingScreenState extends State<SettingScreen> {
     CustomTheme.instanceOf(buildContext).changeTheme(key);
   }
 
+  Future<String> _getVersion() async {
+    final PackageInfo info = await PackageInfo.fromPlatform();
+    return info.version;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,69 +58,81 @@ class _SettingScreenState extends State<SettingScreen> {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                PopUp.showSnackBar(
-                  Text(
-                    'Comming Soon',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: Sizes.dp16(context),
-                      color: ColorPalettes.white,
-                    ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Language'),
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_forward_ios,
+                    size: Sizes.dp16(context),
                   ),
-                  key: scaffoldKey,
+                  onPressed: () {
+                    PopUp.showSnackBar(
+                      Text(
+                        'Comming Soon',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: Sizes.dp16(context),
+                          color: ColorPalettes.white,
+                        ),
+                      ),
+                      key: scaffoldKey,
+                    );
+                  },
+                ),
+              ],
+            ),
+            Divider(),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Theme'),
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_forward_ios,
+                    size: Sizes.dp16(context),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return CustomDialog(
+                          groupValue: _isDark,
+                          onChanged: (value) {
+                            setState(() {
+                              _isDark = value;
+                              _handleRadioValueChange(_isDark);
+                              print('Theme is dark ---> $_isDark');
+                            });
+                          },
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+            Divider(),
+            Spacer(),
+            FutureBuilder<String>(
+              future: _getVersion(),
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                var verInfo = "";
+                if (snapshot.hasData) {
+                  verInfo = "v ${snapshot.data}";
+                }
+                return Container(
+                  margin: EdgeInsets.only(bottom: Sizes.dp30(context)),
+                  child: Text(
+                    verInfo,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 );
               },
-              child: Container(
-                padding: EdgeInsets.only(
-                  top: Sizes.dp10(context),
-                  bottom: Sizes.dp10(context),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text('Language'),
-                    Icon(Icons.arrow_forward_ios),
-                  ],
-                ),
-              ),
             ),
-            Divider(),
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return CustomDialog(
-                        groupValue: _isDark,
-                        onChanged: (value) {
-                          setState(() {
-                            _isDark = value;
-                            _handleRadioValueChange(_isDark);
-                            print('Theme is dark ---> $_isDark');
-                          });
-                        },
-                      );
-                    });
-              },
-              child: Container(
-                padding: EdgeInsets.only(
-                  top: Sizes.dp10(context),
-                  bottom: Sizes.dp10(context),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text('Theme'),
-                    Icon(Icons.arrow_forward_ios),
-                  ],
-                ),
-              ),
-            ),
-            Divider(),
           ],
         ),
       ),
